d7f1793778e9d9dea537d4ed93ef0bbd02bbbee7
Satoriq <sudo.satoriq@gmail.com>
2019-04-15 06:48:01 +0300

    Updates

---
+++ precache-manifest.191d2b908fce7f9f755130231249bf54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce7ce8f74db79aa66f4eab07a1d7e67",
+    "url": "/Movies-App/static/media/logo.fce7ce8f.svg"
+  },
+  {
+    "revision": "42b7c6e119e9ce1d06e9",
+    "url": "/Movies-App/static/js/runtime~main.7da760e7.js"
+  },
+  {
+    "revision": "3ee52e7674d37daa990b",
+    "url": "/Movies-App/static/js/main.70b19dbf.chunk.js"
+  },
+  {
+    "revision": "8d847a10bbfbfc37472e",
+    "url": "/Movies-App/static/js/2.463092ca.chunk.js"
+  },
+  {
+    "revision": "3ee52e7674d37daa990b",
+    "url": "/Movies-App/static/css/main.a0a1a5cf.chunk.css"
+  },
+  {
+    "revision": "8ee4b6f7a993e4d8b1c0b2c3b8deebca",
+    "url": "/Movies-App/index.html"
+  }
+];
